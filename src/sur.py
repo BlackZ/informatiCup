266ab5a8a0a94af322d46dc54d6e921eeee3e297
Jan Pöppel <jpoeppel@InfinityVM.(none)>
2014-11-01 17:26:11 +0100

    extended the first basic test and the corresponding surObject class

--- src/sur.py
+++ src/sur.py
@@ -3,10 +3,21 @@
 Created on Thu Oct 30 13:31:51 2014
 Basic class to load and store space usage rules.
 @author: jpoeppel
+
+Current Problems:
+*Shouldn't ruleName be a list since several rules can apply to the same id?
+*should the ruleName not be split into type and "option" since we have things like smoking=no or access:age="21+"?
 """
 
 class SUR():
   
-  def __init__(self):
-    pass
+  def __init__(self, surID, name, lat, lon):
+    self.id = surID
+    self.ruleName = name
+    self.longitude = lon
+    self.latitude = lat
   
+  @classmethod
+  def fromString(cls, s):
+    data = s.replace(" ", "").split(',')
+    return cls(data[0], data[3], float(data[1]), float(data[2]))
--- src/sur_test.py
+++ src/sur_test.py
@@ -11,14 +11,27 @@ import sur
 class TestSurObject(unittest.TestCase):
   
   def setUp(self):
-    self.ruleName = "smoking=no"
-    self.lat = 61.35274
-    self.long = 48.24532
+    self.testString =("""0001, 50.9304, 5.33901, access:dog="no" """).strip() #Strip the whitespace at the end
+    self.ruleName = ("""access:dog="no" """).strip() #Strip the whitespace at the end
+    self.lat = 50.9304
+    self.long = 5.33901
     self.id = "0001"
   
   def test_createSur(self):
     testSur = sur.SUR(self.id, self.ruleName, self.lat, self.long)
-    assertIsNotNone(testSur)
+    self.assertIsNotNone(testSur)
+    self.assertEqual(testSur.id, self.id)
+    self.assertEqual(testSur.latitude, self.lat)
+    self.assertEqual(testSur.longitude, self.long)
+    self.assertEqual(testSur.ruleName, self.ruleName)
+    
+  def test_parseSur(self):
+    testSur = sur.SUR.fromString(self.testString)
+    self.assertIsNotNone(testSur)
+    self.assertEqual(testSur.id, self.id)
+    self.assertEqual(testSur.latitude, self.lat)
+    self.assertEqual(testSur.longitude, self.long)
+    self.assertEqual(testSur.ruleName, self.ruleName)
     
 
 if __name__ == '__main__':
