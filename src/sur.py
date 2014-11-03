eda6929461b59d4cd94611a1d2826082e0317811
Adriana Dreyer <adreyer@techfak.uni-bielefeld.de>
2014-11-03 21:46:23 +0100

    added new test parseSurFromFile and new function fromFile in sur; rearranged sur-names; added a test file with three entries

--- src/sur.py
+++ src/sur.py
@@ -2,22 +2,57 @@
 """
 Created on Thu Oct 30 13:31:51 2014
 Basic class to load and store space usage rules.
-@author: jpoeppel
-
-Current Problems:
-*Shouldn't ruleName be a list since several rules can apply to the same id?
-*should the ruleName not be split into type and "option" since we have things like smoking=no or access:age="21+"?
+@author: jpoeppel & adreyer
 """
 
 class SUR():
   
   def __init__(self, surID, name, lat, lon):
     self.id = surID
-    self.ruleName = name
-    self.longitude = lon
     self.latitude = lat
-  
+    self.longitude = lon
+    name = name.replace("\"", "").split('=')
+    self.ruleName = {name[0]: name[1]}
+    
+  ##
+  # addRuleName adds another rule name to the dictionary
+  def addRuleName(self, name):
+    name = name.replace("\"", "").split('=')
+    self.ruleName[name[0]] = name[1]
+    
+  ##
+  # fromString builds an instance of SUR given a string containing the
+  # relevant data.
   @classmethod
   def fromString(cls, s):
     data = s.replace(" ", "").split(',')
     return cls(data[0], data[3], float(data[1]), float(data[2]))
+    
+  ##
+  # fromFile builds multiple instances of SUR given a file containing the
+  # relevant data.
+  @classmethod
+  def fromFile(cls, f):
+    # open file and read first line (num of SURs)
+    num = f.readline()
+    try:
+      num=int(num)
+    except ValueError:
+      print "Invalid data file: First line is no number."
+    
+    # build SURs
+    surs = []
+    lastID = 0
+    for i in range(int(num)):
+      line = f.readline().replace(" ","").split(',')
+      if line[0] == lastID:
+        # only 'new' sur with equal id/lat/long
+        surs[-1].addRuleName(line[3].rstrip())
+      else:
+        # completely new sur
+        sur = cls(line[0], line[3].rstrip(), float(line[1]), float(line[2]))
+        surs.append(sur)
+        lastID = line[0]
+      
+    #return SURs
+    return surs
--- src/test_sur.py
+++ src/test_sur.py
@@ -2,7 +2,7 @@
 """
 Created on Thu Oct 30 13:20:21 2014
 
-@author: jpoeppel
+@author: jpoeppel & adreyer
 """
 
 import unittest
@@ -11,14 +11,15 @@ import sur
 class TestSurObject(unittest.TestCase):
   
   def setUp(self):
-    self.testString =("""0001, 50.9304, 5.33901, access:dog="no" """).strip() #Strip the whitespace at the end
-    self.ruleName = ("""access:dog="no" """).strip() #Strip the whitespace at the end
+    self.testString =("0001, 50.9304, 5.33901, access:dog=\"no\"")
+    self.id = "0001"
     self.lat = 50.9304
     self.long = 5.33901
-    self.id = "0001"
+    self.ruleNameString = "access:dog=\"no\""
+    self.ruleName = {"access:dog": "no"}
   
   def test_createSur(self):
-    testSur = sur.SUR(self.id, self.ruleName, self.lat, self.long)
+    testSur = sur.SUR(self.id, self.ruleNameString, self.lat, self.long)
     self.assertIsNotNone(testSur)
     self.assertEqual(testSur.id, self.id)
     self.assertEqual(testSur.latitude, self.lat)
@@ -33,6 +34,30 @@ class TestSurObject(unittest.TestCase):
     self.assertEqual(testSur.longitude, self.long)
     self.assertEqual(testSur.ruleName, self.ruleName)
     
+  def test_parseSurFromFile(self):
+    # set up
+    testFile = open('TestData.txt','r')
+    ruleName1_2 = {"smoking": "no", "access:dog": "no"}
+    id2 = "0002"
+    lat2 = 50.9325
+    long2 = 5.34174
+    ruleName2 = {"cellphone": "no"}
+    
+    # do it!
+    testSurs = sur.SUR.fromFile(testFile)
+    
+    # check first sur
+    self.assertIsNotNone(testSurs)
+    self.assertEqual(testSurs[0].id, self.id)
+    self.assertEqual(testSurs[0].latitude, self.lat)
+    self.assertEqual(testSurs[0].longitude, self.long)
+    self.assertEqual(testSurs[0].ruleName, ruleName1_2)
+    # check second sur
+    self.assertEqual(testSurs[1].id, id2)
+    self.assertEqual(testSurs[1].latitude, lat2)
+    self.assertEqual(testSurs[1].longitude, long2)
+    self.assertEqual(testSurs[1].ruleName, ruleName2)
+
 
 if __name__ == '__main__':
   unittest.main()
