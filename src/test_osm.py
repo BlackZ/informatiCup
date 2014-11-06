5ef85ab7d56446cf4be17a37de6ba32f78eaa31f
BlackZ <jpoeppel@techfak.uni-bielefeld.de>
2014-11-06 11:49:36 +0100

    created empty test classes for osm data and api wrapper

---
+++ src/test_osm.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  6 11:46:54 2014
+
+@author: jpoeppel
+"""
+
---
+++ src/test_osmAPI.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  6 11:47:29 2014
+
+@author: jpoeppel
+"""
+
