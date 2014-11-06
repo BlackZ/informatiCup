bb878d5f46173ea605d7f364b7d5a61cff4d2a48
BlackZ <jpoeppel@techfak.uni-bielefeld.de>
2014-11-06 11:57:29 +0100

    added empty test files for the kmlComperator and the pipeline

---
+++ src/test_kmlComperator.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  6 11:52:04 2014
+
+@author: jpoeppel
+"""
+
---
+++ src/test_pipeline.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  6 11:53:07 2014
+
+@author: jpoeppel
+"""
+
