ef80e8bcdadd8f1f9975fbe00b2df3bcf6365b41
tschodde <tschodde@techfak.uni-bielefeld.de>
2014-11-07 14:52:50 +0100

    xml parsen angefangen

--- src/osmAPI.py
+++ src/osmAPI.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import requests
+import osmData
+from xml.dom.minidom import parse, parseString
 
 class osmAPI():
   def __init__(self):
@@ -10,9 +12,21 @@ class osmAPI():
     return {'data': '[out:xml][timeout:25];(node[""=""]({minLat},{minLon},{maxLat},{maxLon});way[""=""]({minLat},{minLon},{maxLat},{maxLon});relation[""=""]({minLat},{minLon},{maxLat},{maxLon}););out body;>;out skel qt;'.format(**locals())}
 
   def performRequest(self,boundingBox):
-    return requests.get(self.osmurl,params=self.getOsmRequestData(boundingBox[0],boundingBox[1],boundingBox[2],boundingBox[3]))
+    return self.parseData(requests.get(self.osmurl,params=self.getOsmRequestData(boundingBox[0],boundingBox[1],boundingBox[2],boundingBox[3])))
   
-  def name(self, ):
-    pass
+  def parseData(self, obj):
+    osmObj=osmData.OSM()
+    dom = parseString(obj.content)
+    for node in dom.getElementsByTagName('node'):
+      #müssen noch irgendwo weggesucht werden.
+      tags={}
+      nodeObj=osmData.Node(node.attributes['id'],node.attributes['lat'],node.attributes['long'],tags)
+    for way in dom.getElementsByTagName('way'):
+      pass
+    for relation in dom.getElementsByTagName('relation'):
+      pass
     
-    
+if __name__ == '__main__':
+  obj=osmAPI()
+  boundingBox=[52.032736,8.486593,52.042113,8.501194]
+  obj.performRequest(boundingBox)
--- src/playground/datenLesen.py
+++ src/playground/datenLesen.py
@@ -15,11 +15,11 @@ minLat=boundingBox[1];
 maxLon=boundingBox[2];
 maxLat=boundingBox[3];
 
-#osmrequest = {'data': '[out:xml][timeout:25];(node["amenity"="university"](%s,%s,%s,%s);way["amenity"="university"](%s,%s,%s,%s);relation["amenity"="university"](%s,%s,%s,%s););out body;>;out skel qt;'%(minLat, minLon, maxLat, maxLon, minLat, minLon, maxLat, maxLon, minLat, minLon, maxLat, maxLon)}
-osmrequest = {'data': '[out:xml][timeout:25];(node[""=""]({minLat},{minLon},{maxLat},{maxLon});way[""=""]({minLat},{minLon},{maxLat},{maxLon});relation[""=""]({minLat},{minLon},{maxLat},{maxLon}););out body;>;out skel qt;'.format(**locals())}
+osmrequest = {'data': '[out:xml][timeout:25];(node["amenity"="university"](%s,%s,%s,%s);way["amenity"="university"](%s,%s,%s,%s);relation["amenity"="university"](%s,%s,%s,%s););out body;>;out skel qt;'%(minLat, minLon, maxLat, maxLon, minLat, minLon, maxLat, maxLon, minLat, minLon, maxLat, maxLon)}
+#osmrequest = {'data': '[out:xml][timeout:25];(node[""=""]({minLat},{minLon},{maxLat},{maxLon});way[""=""]({minLat},{minLon},{maxLat},{maxLon});relation[""=""]({minLat},{minLon},{maxLat},{maxLon}););out body;>;out skel qt;'.format(**locals())}
 
 osmurl='http://overpass-api.de/api/interpreter'
-#osm=requests.get(osmurl,params=osmrequest)
+osm=requests.get(osmurl,params=osmrequest)
 
 osmdata=osm.content
-print(osmdata)
+#print(osmdata)
