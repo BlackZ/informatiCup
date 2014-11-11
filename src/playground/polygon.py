fc0937b974dc79b873ead115cb1da7e059ca9b0a
tschodde <tschodde@techfak.uni-bielefeld.de>
2014-11-11 15:05:49 +0100

    Played with Adrianas Polygon :smile:

--- src/playground/polygon.py
+++ src/playground/polygon.py
@@ -13,6 +13,13 @@ p2=Polygon.Polygon(((3.0,3.0),(9.0,0.0),(12.0,4.0),(10.0,9.0)))
 
 p3=p1&p2
 
+#test, if point is inside a polygon
+#this function cant test, whether the point is in the border or not
+print "Inside: ", p1.isInside(5.0, 6.0)
+print "On Border: ", p1.isInside(0.0, 4.0)
+print "On Border: ", p1.isInside(6.0, 3.0)
+print "Outside: ", p1.isInside(0.0, 2.0)
+
 print(p1.area())
 print(p2.area())
 print(p3.area())
