eb45c958378dc8c0a1e02b74348001d6459d3c81
Adriana Dreyer <adreyer@techfak.uni-bielefeld.de>
2014-12-31 16:40:32 +0100

    added file necessary for packaging

---
+++ setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 29 22:59:36 2014
+
+@author: adreyer
+"""
+
+from distutils.core import setup
+
+setup( 
+     name = "isySUR", 
+     version = "1.0", 
+     author = "Adriana-Victoria Dreyer, Jacqueline Hemminghaus, Jan Pöppel, Thorsten Schodde", 
+     author_email = "adreyer@techfak.uni-bielefeld.de, jhemming@techfak.uni-bielefeld.de, jpoeppel@techfak.uni-bielefeld.de, tschodde@techfak.uni-bielefeld.de", 
+     scripts = ["run_isySUR.py"],
+     packages = ["isySUR"]
+     )
