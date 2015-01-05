7aee2696e50cb2c8f2bb0e925b83ee179d65020f
jhemming <j.hemminghaus@gmx.de>
2015-01-05 22:18:18 +0100

    We have a Toaster :D but colors have to be changed

--- isySUR/gui/MapGUI.py
+++ isySUR/gui/MapGUI.py
@@ -7,13 +7,19 @@ from isySUR import kmlData
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.button import Button
-from kivy.properties import ObjectProperty
+from kivy.properties import ObjectProperty, StringProperty, NumericProperty,\
+                          ListProperty
 from kivy.uix.textinput import TextInput
 from kivy.uix.popup import Popup
 from kivy.uix.dropdown import DropDown
+from kivy.uix.label import Label
+from kivy.clock import Clock
+from kivy.graphics import Color, Rectangle, Line
 from mapview import MapView
 
 class Map(FloatLayout):
+  
+  text = StringProperty()
   
   def __init__(self):
     super(Map, self).__init__()
@@ -26,6 +32,9 @@ class Map(FloatLayout):
     self.menue = Menue()
     
     self.kmlList = KMLList()
+  
+  def toast(self, text):
+    Toast().show(text, False)
       
   def open_menue(self):
     if self.menue.isOpen:
@@ -36,7 +45,8 @@ class Map(FloatLayout):
     
   def open_kmlList(self):
     if len(app.loaded_kmls) == 0:
-      self.ids.toast.text = "No KML files loaded!"
+      self.toast("No KML files loaded!")
+      #self.ids.toast.text = "No KML files loaded!"
     elif self.kmlList.is_open:
       self.kmlList.dismiss(self.ids.kml)
     else:
@@ -90,7 +100,8 @@ class Menue(DropDown):
         map_view.kmlList.addItem(item_name)
         map_view.addPolygon(polyList)
       except:
-        map_view.ids.toast.text = "The loaded file is incomplete!"
+        map_view.toast('The loaded file is incomplete!')
+        #map_view.ids.toast.text = "The loaded file is incomplete!"
   
     self.dismiss_popup()
   
@@ -102,7 +113,8 @@ class Menue(DropDown):
             stream.write(app.loaded_kmls[kml]['data'].getXML())
       except Exception as e:
         print e
-        map_view.ids.toast.text = "An error occured while saving!"
+        map_view.toast('An error occured while saving!')
+        #map_view.ids.toast.text = "An error occured while saving!"
 
     self.dismiss_popup()
 
@@ -139,7 +151,42 @@ class KMLList(DropDown):
     btn = Button(text=name, size_hint_y=None, height=44,background_color=(0,0,2,1))
     btn.bind(on_release=self.selectBut)
     self.add_widget(btn)
+
+class Toast(Label):
+  _transparency = NumericProperty(1.0)
+  _background = ListProperty((0, 0, 0, 1))
+  
+  def __init__(self):
+    super(Toast, self).__init__()
+    self.pos = (map_view.center_x - self.width/2, 0.075)
+  
+  def show(self, text, length_long):
+    duration = 5000 if length_long else 1000
+    rampdown = duration * 0.1
+    if rampdown > 500:
+      rampdown = 500
+    if rampdown < 100:
+      rampdown = 100
     
+    self._rampdown = rampdown
+    self._duration = duration - rampdown
+    self.text = text
+    self.texture_update()
+    map_view.add_widget(self)
+    with self.canvas.before:
+      Color(0,0,0,1)
+      Rectangle(pos=(map_view.center_x - self.texture_size[0]/2 - 2.5, 10), size=(self.texture_size[0] + 5, self.texture_size[1]+ 2))
+    
+    Clock.schedule_interval(self._in_out, 1/60.0)
+  
+  def _in_out(self, dt):
+    self._duration -= dt*1000
+    if self._duration <= 0:
+      self._transparency = 1.0 + (self._duration / self._rampdown)
+    if -(self._duration) > self._rampdown:
+      map_view.remove_widget(self)
+      return False
+  
 class LoadDialog(FloatLayout):
   load = ObjectProperty(None)
   cancel = ObjectProperty(None)
