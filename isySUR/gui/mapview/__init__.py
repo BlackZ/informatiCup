615f83bbdff29900574232f8c3b83883eb1a3fb7
Adriana Dreyer <adreyer@techfak.uni-bielefeld.de>
2015-01-09 16:15:05 +0100

    corrected space number, removed unused imports, added sqrt import in triangulation

--- isySUR/gui/mapview/__init__.py
+++ isySUR/gui/mapview/__init__.py
@@ -9,7 +9,7 @@ MapView is a Kivy widget that display maps.
 """
 
 __all__ = ["Coordinate", "Bbox", "MapView", "MapSource", "MapMarker",
-           "MapLayer", "MarkerMapLayer", "MapMarkerPopup"]
+       "MapLayer", "MarkerMapLayer", "MapMarkerPopup"]
 __version__ = "0.2"
 
 MIN_LATITUDE = -90.
@@ -18,30 +18,29 @@ MIN_LONGITUDE = -180.
 MAX_LONGITUDE = 180.
 
 try:
-    import os
-    from kivy import platform
+  import os
+  from kivy import platform
+  
+  if "win" == platform:
+    CACHE_DIR = os.path.expanduser('~') + "\AppData\Local\Temp\isySUR"
+  elif "linux" == platform or "macosx" == platform:
+    CACHE_DIR = '/tmp/isySUR'
+  else:
+    CACHE_DIR = 'cache'
     
-    if "win" == platform:
-        CACHE_DIR = os.path.expanduser('~') + "\AppData\Local\Temp\isySUR"
-    elif "linux" == platform or "macosx" == platform:
-        CACHE_DIR = '/tmp/isySUR'
-    else:
-        CACHE_DIR = 'cache'
-        
-    del os
-    del sys
+  del os
 except:
-    pass
+  pass
 
 try:
-    # fix if used within our programm
-    import sys
-    sys.modules['mapview'] = sys.modules['isySUR.gui.mapview']
-    del sys
+  # fix if used within our programm
+  import sys
+  sys.modules['mapview'] = sys.modules['isySUR.gui.mapview']
+  del sys
 except KeyError:
-    pass
+  pass
 
 from mapview.types import Coordinate, Bbox
 from mapview.source import MapSource
 from mapview.view import MapView, MapMarker, MapLayer, MarkerMapLayer, \
-    MapMarkerPopup
+  MapMarkerPopup
--- isySUR/gui/mapview/utils.py
+++ isySUR/gui/mapview/utils.py
@@ -3,4 +3,4 @@
 __all__ = ["clamp"]
 
 def clamp(x, minimum, maximum):
-    return max(minimum, min(x, maximum))
+  return max(minimum, min(x, maximum))
