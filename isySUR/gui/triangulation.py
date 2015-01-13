4c81f19b23c63eda558d9784327f4fde19d2e708
BlackZ <jpoeppel@techfak.uni-bielefeld.de>
2015-01-13 13:11:38 +0100

    Added pydocs for triangulation.py. Closes #92

--- isySUR/gui/triangulation.py
+++ isySUR/gui/triangulation.py
@@ -9,12 +9,34 @@ but not using pymunk and more flexible to the polygon orientation.
 """
 
 class Triangulator():
+  """
+    A simple triangulator. Takes a polygon outline with no self intersection or holes and
+    computes a triangulation using ear clipping. 
+  """
   
   def __init__(self):
+    """
+      Constructor for the triangulator. Initialises the clockwise variable, used to store if the polygon
+      points are ordered clockwise or counter-clockwise
+    """
     self.clockwise = False
 
   def _is_corner(self,a,b,c):
-     # returns if point b is an outer corner
+    """
+      Private function to compute if point b is a corner point.
+      
+      @param a: First point of potential corner
+      @type a: Tupel(Float,Float)
+      
+      @param b: First point of potential corner
+      @type b: Tupel(Float,Float)
+      
+      @param c: First point of potential corner
+      @type c: Tupel(Float,Float)
+      
+      @return: True if point b is a corner point, else False
+      @rtype: Boolean
+    """
     if self.clockwise:
       return self.is_clockwise([a,b,c])
     else:
@@ -23,15 +45,34 @@ class Triangulator():
 
                  
   def calc_area(self, tri):
+    """
+      Function to compute the given triangle.
+      
+      @param tri: The triangle which area is to be computed
+      @type tri: List[a,b,c]
+      
+      @return: Area of the triangle, if there were 3 points given in tri. None otherwise
+      @rtype: Float
+    """
     if len(tri) == 3:
       edge1 = (tri[1][0] -tri[0][0], tri[1][1]-tri[0][1])
       edge2 = (tri[2][0] -tri[0][0], tri[2][1]-tri[0][1])
       return 0.5 * (edge1[0]*edge2[1] - edge2[0]*edge1[1])
     else:
       print "not a triangle!!"
+      return None
   
   
   def is_clockwise(self, points):
+    """
+      Function to compute if the orientation of a pointlist is clockwise.
+      
+      @param points: The pointlist that is to be checked.
+      @type points: [(Float,Float),]
+      
+      @return: True if the points are orientated clockwise, false if they are orientated counter-clockwise.
+      @rtype: Boolean
+    """
     edgeSum = 0
     size = len(points)
     for i in range(size):
@@ -43,6 +84,24 @@ class Triangulator():
 
 
   def _point_in_triangle(self, p,a,b,c):
+    """
+      Private function to test if a point p is insie a given triange.
+      
+      @param p: The point to be tested.
+      @type p: Tupel(Float,Float)
+      
+      @param a: The first point of the triangle that is to be tested against.
+      @type a: Tupel(Float,Float)
+      
+      @param b: The first point of the triangle that is to be tested against.
+      @type b: Tupel(Float,Float)
+      
+      @param c: The first point of the triangle that is to be tested against.
+      @type c: Tupel(Float,Float)
+      
+      @return: True if the point p is inside the triangle a,b,c, else False.
+      @rtype: Boolean
+    """
     # measure area of whole triangle
     whole = abs(self.calc_area([a,b,c]))
     # measure areas of inner triangles formed by p
@@ -57,6 +116,17 @@ class Triangulator():
     return ((parta+partb+partc) < (whole+thresh))
       
   def _get_ear(self, poly):
+    """
+      Private function to get an ear of the given polygon. An ear is a triangle of three consecutive
+      points, where only one edge is inside the triange.
+      
+      @param poly: The pointlist making up the polygon.
+      @type poly: [Tupel(Float,Float),]
+      
+      @return: A list containing the 3 points ear and a list containing the remaining polygon. Returns empty
+              lists, if no ear was found.
+      @rtype: [Tupel(Float,Float),Tupel(Float,Float),Tupel(Float,Float)], [Tupel(Float,Float),]
+    """
     count = len(poly)
     # not even a poly
     if count < 3:
@@ -97,15 +167,18 @@ class Triangulator():
    
   def triangulate(self,poly):
     """
-    triangulates poly and returns a list of triangles
-    poly: list of points that form an anticlockwise polygon (self-intersecting polygons won't work, results are... undefined)
+      Main function performing the triangulation of the given polygon. Does not work for self intersecting
+      polygons!
+      
+      @param poly: Pointlist making up the polygon. Points can be sorted either clockwise or counter-clockwise.
+      @type poly: [Tupel(Float,Float),]
+      
+      @return: List of triangles covering the polygon.
+      @rtype: [[Tupel(Float,Float),Tupel(Float,Float),Tupel(Float,Float)], ]
     """
     triangles = []
     remaining = poly[:]
-    if self.is_clockwise(poly):
-      self.clockwise = True
-    else:
-      self.clockwise = False
+    self.clockwise = self.is_clockwise(poly)
     # while the poly still needs clipping
     while len(remaining) > 2:
       # rotate the list:
