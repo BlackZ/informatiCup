36373a2ea95cec6795562bf169dba0e7589bedf9
Adriana Dreyer <adreyer@techfak.uni-bielefeld.de>
2015-01-15 12:47:53 +0100

    added howto for building apk

---
+++ android/main.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 05 18:38:46 2015
+Main entrance point android app
+@author: adreyer
+"""
+
+if __name__ == '__main__':
+  import isySUR.gui.MapGUI as gui
+  gui.MapApp().run()
